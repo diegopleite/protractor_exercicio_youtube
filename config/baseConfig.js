a73fb9eb945e477e548cd658693c9c1a955b8d3e
Diego Pires Leite <diego.leite@linx.com.br>
2022-05-23 21:45:44 -0300

    jasmine-spec-reporter

--- config/baseConfig.js
+++ config/baseConfig.js
@@ -1,13 +1,36 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
     framework: "jasmine",
     seleniumAddress: "http://localhost:4444/wd/hub",
 
     specs: [
-        //"../specs/spec.js",
+        "../specs/spec.js",
         "../specs/spec.others.js"        
     ],
     baseUrl : 'https://blog.taller.net.br/',
     capabilities:{
         browserName:"chrome"
-    }
+    },
+
+    // onPrepare: function () {
+    //   let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+    //   jasmine.getEnv().addReporter(new SpecReporter({
+    //     spec: {
+    //       displayStacktrace: all
+    //     },
+    //     summary: {
+    //       displayDuration: true
+    //     }
+    //   }));
+    // }  
+    onPrepare: function(){
+      jasmine.getEnv().addReporter(new SpecReporter({
+        displayFailuresSummary: true,
+        displayFailuredSpec: true,
+        displaySuiteNumber: true,
+        displaySpecDuration: true
+      }));
+    } 
 }
